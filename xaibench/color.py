2874979b561ec5546711d0e8f342566ccbafe06a
Jose Jimenez <hawk31@gmail.com>
2021-03-26 15:24:38 +0100

    batching colors

--- xaibench/color.py
+++ xaibench/color.py
@@ -13,6 +13,7 @@ from graph_attribution.graphnet_techniques import (
     AttentionWeights,
 )
 from graph_nets.graphs import GraphsTuple
+from graph_attribution.graphs import get_num_graphs, get_graphs_tf
 
 from xaibench.utils import DATA_PATH, MODELS_PATH
 
@@ -37,7 +38,18 @@ def ig_ref(g):
     return g_ref
 
 
-def color_pairs(pair_f, block_type="gcn"):
+def get_batch_indices(n: int, batch_size: int):
+    n_batches = n // batch_size
+    indices = tf.range(n)
+    if n < batch_size:
+        indices = tf.reshape(indices, (1, n))
+        return indices
+    indices = indices[: n_batches * batch_size]
+    indices = tf.reshape(indices, (n_batches, batch_size))
+    return indices
+
+
+def color_pairs(pair_f, batch_size=16, block_type="gcn"):
     id_ = os.path.basename(os.path.dirname(pair_f))
 
     colors_pt = os.path.join(os.path.dirname(pair_f), "colors.pt")
@@ -48,32 +60,41 @@ def color_pairs(pair_f, block_type="gcn"):
     df = pd.read_csv(pair_f)
     tensorizer = MolTensorizer()
 
-    with tf.device("/GPU:0"):
-        g_i, g_j = (
-            smiles_to_graphs_tuple(df["smiles_i"], tensorizer),
-            smiles_to_graphs_tuple(df["smiles_j"], tensorizer),
-        )
+    g_i, g_j = (
+        smiles_to_graphs_tuple(df["smiles_i"], tensorizer),
+        smiles_to_graphs_tuple(df["smiles_j"], tensorizer),
+    )
 
-        colors = {}
-        with open(os.path.join(MODELS_PATH, f"{block_type}_{id_}.pt"), "rb") as handle:
-            model = dill.load(handle)
+    colors = {}
 
-        for col_method in AVAIL_METHODS:
-            extra_kwargs = {}
+    with open(os.path.join(MODELS_PATH, f"{block_type}_{id_}.pt"), "rb") as handle:
+        model = dill.load(handle)
 
-            if col_method == AttentionWeights and block_type != "gat":
-                continue
+    for col_method in AVAIL_METHODS:
+        extra_kwargs = {}
+        col_i = []
+        col_j = []
 
-            if col_method == IntegratedGradients:
-                extra_kwargs["num_steps"] = 500
-                extra_kwargs["reference_fn"] = ig_ref
+        if col_method == AttentionWeights and block_type != "gat":
+            continue
 
-            col_i, col_j = (
-                col_method(**extra_kwargs).attribute(g_i, model),
-                col_method(**extra_kwargs).attribute(g_j, model),
-            )
-            assert len(col_i) == len(col_j)
-            colors[col_method.__name__] = [(c_i, c_j) for c_i, c_j in zip(col_i, col_j)]
+        if col_method == IntegratedGradients:
+            extra_kwargs["num_steps"] = 500
+            extra_kwargs["reference_fn"] = ig_ref
+
+        n = get_num_graphs(g_i)
+        indices = get_batch_indices(n, batch_size)
+        for idx_i, idx_j in zip(indices, indices):
+            with tf.device("/GPU:0"):
+                b_i, b_j = get_graphs_tf(g_i, idx_i), get_graphs_tf(g_j, idx_j)
+                c_i = col_method(**extra_kwargs).attribute(b_i, model)
+                c_j = col_method(**extra_kwargs).attribute(b_j, model)
+
+            col_i.extend(c_i)
+            col_j.extend(c_j)
+
+        assert len(col_i) == len(col_j)
+        colors[col_method.__name__] = [(c_i, c_j) for c_i, c_j in zip(col_i, col_j)]
     return colors
 
 
