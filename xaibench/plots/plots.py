1a7cbab11307bd9e3c420e1682d1976616a70c43
Jose Jimenez <hawk31@gmail.com>
2021-06-09 11:48:55 +0200

    organizing project differently

--- xaibench/plots/plots.py
+++ xaibench/plots/plots.py
@@ -1,187 +1,23 @@
+import collections
 import os
 from glob import glob
 
 import dill
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.font_manager import FontProperties
-from rdkit.Chem import MolFromSmiles
-from sklearn.metrics import accuracy_score
-from tqdm import tqdm
-
 from xaibench.color import AVAIL_METHODS
 from xaibench.determine_col import MIN_PER_COMMON_ATOMS
-from xaibench.utils import (BLOCK_TYPES, DATA_PATH, FIG_PATH, LOG_PATH,
-                            RESULTS_PATH)
-
-N_THRESHOLDS = len(MIN_PER_COMMON_ATOMS)
-
-matplotlib.use("Agg")
-
-
-def color_agreement(color_true, color_pred, metric_f):
-    """
-    Checks agreement between true and predicted colors.
-    """
-    assert len(color_true) == len(color_pred)
-    idx_noncommon = [idx for idx, val in color_true.items() if val != 0.0]
-    if len(idx_noncommon) == 0:
-        return -1.0
-    color_true_noncommon = np.array([color_true[idx] for idx in idx_noncommon])
-    color_pred_noncommon = np.sign([color_pred[idx] for idx in idx_noncommon])
-    return metric_f(color_true_noncommon, color_pred_noncommon)
-
-
-def distribute_bonds(cm, mol):
-    """
-    Distributes bond importances evenly across their connecting nodes.
-    """
-    atom_imp = cm.nodes.numpy()
-    bond_imp = np.array([b for idx, b in enumerate(cm.edges.numpy()) if idx % 2 == 0])
-
-    for bond_idx, bond in enumerate(mol.GetBonds()):
-        b_imp = bond_imp[bond_idx] / 2
-        atom_imp[bond.GetBeginAtomIdx()] += b_imp
-        atom_imp[bond.GetEndAtomIdx()] += b_imp
-    return atom_imp
-
-
-# TODO: this function needs to be refactored
-def method_comparison(colors_path, avail_methods=None, assign_bonds=False):
-    avg_scores = {}
-    idx_valid = {}
-
-    for idx, color_method_f in enumerate(tqdm(colors_path)):
-        dirname = os.path.dirname(color_method_f)
-
-        with open(os.path.join(dirname, "colors.pt"), "rb") as handle:
-            colors = dill.load(handle)
-
-        with open(color_method_f, "rb") as handle:
-            manual_colors = dill.load(handle)
-
-        # If bond importances are used, bond information from the mols is needed
-        if assign_bonds:
-            pair_df = pd.read_csv(os.path.join(dirname, "pairs.csv"))
-            mols = [
-                (MolFromSmiles(mi), MolFromSmiles(mj))
-                for mi, mj in zip(pair_df["smiles_i"], pair_df["smiles_j"])
-            ]
-            assert len(mols) == len(colors)
-
-        for method in avail_methods if avail_methods is not None else ["rf"]:
-            if avail_methods is not None:
-                if method == "diff":
-                    method_name = method
-                else:
-                    method_name = method.__name__
-
-                if assign_bonds and method_name != "diff":
-                    colors_method = [
-                        (
-                            distribute_bonds(cm_pair[0], mol_pair[0]),
-                            distribute_bonds(cm_pair[1], mol_pair[1]),
-                        )
-                        for cm_pair, mol_pair in zip(manual_colors[method_name], mols)
-                    ]
-                elif method_name != "diff":
-                    colors_method = [
-                        (cm[0].nodes.numpy(), cm[1].nodes.numpy())
-                        for cm in manual_colors[method_name]
-                    ]
-                else:
-                    colors_method = manual_colors[method_name]
-
-            else:
-                colors_method = manual_colors
-                method_name = method
-
-            assert len(colors) == len(colors_method)
-
-            for idx_th in range(N_THRESHOLDS):
-                colors_th = [col[idx_th] for col in colors]
-
-                if sum(1 for _ in filter(None.__ne__, colors_th)) > 0:
-                    scores = []
-                    for color_pair_true, color_pair_pred in zip(
-                        colors_th, colors_method
-                    ):
-                        if color_pair_true is not None:
-                            ag_i = color_agreement(
-                                color_pair_true[0],
-                                color_pair_pred[0],
-                                metric_f=accuracy_score,
-                            )
-                            scores.append(ag_i)
-                            ag_j = color_agreement(
-                                color_pair_true[1],
-                                color_pair_pred[1],
-                                metric_f=accuracy_score,
-                            )
-                            scores.append(ag_j)
-
-                    scores = np.array(scores)
-                    scores = scores[
-                        scores >= 0.0
-                    ]  # Filter examples with non-common MCS
-
-                    if scores.size > 0:
-                        avg_scores.setdefault(
-                            method_name, [[] for _ in range(N_THRESHOLDS)]
-                        )[idx_th].append(scores.mean())
-                        idx_valid.setdefault(
-                            method_name, [[] for _ in range(N_THRESHOLDS)]
-                        )[idx_th].append(idx)
-    return avg_scores, idx_valid
-
+from xaibench.score import N_THRESHOLDS
+from xaibench.utils import BLOCK_TYPES, DATA_PATH, FIG_PATH, RESULTS_PATH, LOG_PATH
 
 if __name__ == "__main__":
-    os.makedirs(RESULTS_PATH, exist_ok=True)
-    os.makedirs(FIG_PATH, exist_ok=True)
-    results_path = os.path.join(RESULTS_PATH, "scores.pt")
-    idxs_path = os.path.join(RESULTS_PATH, "idxs.pt")
+    with open(os.path.join(RESULTS_PATH, "scores.pt"), "rb") as handle:
+        scores = dill.load(handle)
 
-    colors_rf = glob(os.path.join(DATA_PATH, "validation_sets", "*", "colors_rf.pt"))
-
-    if not (os.path.exists(results_path) and os.path.exists(idxs_path)):
-        print("Computing scores...")
-        scores = {}
-        idxs = {}
-        scores["rf"] = {}
-        idxs["rf"] = {}
-
-        scores["rf"], idxs["rf"] = method_comparison(colors_rf)
-
-        for bt in BLOCK_TYPES:
-            print(f"Now loading block type {bt}...")
-            avail_methods = AVAIL_METHODS if bt == "gat" else AVAIL_METHODS[:-1]
-            avail_methods = avail_methods + [
-                "diff"
-            ]  # TODO: rewrite this more elegantly
-
-            colors_method = glob(
-                os.path.join(DATA_PATH, "validation_sets", "*", f"colors_{bt}.pt",)
-            )
-
-            scores[bt], idxs[bt] = method_comparison(
-                colors_method, avail_methods, assign_bonds=True
-            )
-
-        with open(results_path, "wb") as handle:
-            dill.dump(scores, handle)
-
-        with open(idxs_path, "wb") as handle:
-            dill.dump(idxs, handle)
-
-    else:
-        with open(results_path, "rb") as handle:
-            scores = dill.load(handle)
-
-        with open(idxs_path, "rb") as handle:
-            idxs = dill.load(handle)
-
+    with open(os.path.join(RESULTS_PATH, "idxs.pt"), "rb") as handle:
+        idxs = dill.load(handle)
 
     # median plot
     f, ax = plt.subplots(figsize=(8, 8))
@@ -230,7 +66,7 @@ if __name__ == "__main__":
     similarities_rf = []
     exists_rf = []
 
-    colors_rf = np.array(colors_rf)[idxs["rf"]["rf"][0]]
+    colors_rf = np.array(scores["rf"])[idxs["rf"]["rf"][0]]
 
     for idx, color_f in enumerate(colors_rf):
         sim_file = os.path.join(os.path.dirname(color_f), "similarity.npy")
@@ -334,7 +170,9 @@ if __name__ == "__main__":
 
 
     # performance
-    losses_rf = []
+
+    all_metrics = {}
+    all_metrics["rf"] = collections.defaultdict(list)
     exists_rf = []
 
     for idx, color_f in enumerate(colors_rf):
@@ -342,10 +180,13 @@ if __name__ == "__main__":
         metrics_path = os.path.join(LOG_PATH, f"{id_}_metrics_rf.pt")
         if os.path.exists(metrics_path):
             with open(metrics_path, "rb") as handle:
-                losses_rf.append(dill.load(handle)[0])
+                metrics = dill.load(handle)
+                all_metrics["rf"]["rmse_train"].append(metrics["rmse_train"])
+                all_metrics["rf"]["rmse_test"].append(metrics["rmse_test"])
+                all_metrics["rf"]["pcc_train"].append(metrics["pcc_train"])
+                all_metrics["rf"]["pcc_test"].append(metrics["pcc_test"])
             exists_rf.append(idx)
 
-    losses_rf = np.array(losses_rf)
     y_rf = np.array(scores["rf"]["rf"][0])[exists_rf]
 
     for bt in BLOCK_TYPES:
@@ -368,7 +209,7 @@ if __name__ == "__main__":
             ),
         )
 
-        losses = []
+        all_metrics[bt] = collections.defaultdict(list)
 
         colors_bt = np.array(
             glob(os.path.join(DATA_PATH, "validation_sets", "*", f"colors_{bt}.pt",))
@@ -378,9 +219,13 @@ if __name__ == "__main__":
             id_ = os.path.basename(os.path.dirname(color_f))
 
             with open(os.path.join(LOG_PATH, f"{bt}_{id_}.pt"), "rb") as handle:
-                losses.append(dill.load(handle)["rmse_train"][-1])
+                metrics = dill.load(handle)
+            
+            all_metrics[bt]["rmse_train"].append(metrics["rmse_train"][-1])
+            all_metrics[bt]["rmse_test"].append(metrics["rmse_test"][-1])
+            all_metrics[bt]["pcc_train"].append(metrics["pcc_train"][-1])
+            all_metrics[bt]["pcc_test"].append(metrics["pcc_test"][-1])
 
-        losses = np.array(losses)
         for idx_m, method in enumerate(avail_methods):
             method_name = method if isinstance(method, str) else method.__name__
             y = np.array(scores[bt][method_name][0])
