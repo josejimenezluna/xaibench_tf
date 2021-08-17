b0363255faf9d182c6da1136fd03918cf3753356
Jose Jimenez <hawk31@gmail.com>
2021-08-17 10:57:17 +0200

    overlap script. updated desc plot

--- xaibench/explore/overlap.py
+++ xaibench/explore/overlap.py
@@ -1,59 +1,26 @@
 import os
 from glob import glob
 
-import dill
 import numpy as np
 import pandas as pd
-from rdkit.Chem import MolFromSmiles, MolToInchi
-from rdkit.Chem.MolStandardize import rdMolStandardize
-from rdkit import rdBase
 from tqdm import tqdm
 from xaibench.utils import DATA_PATH, RESULTS_PATH
 
-rdBase.DisableLog("rdApp.*")
-
 if __name__ == "__main__":
-    train_csvs = glob(os.path.join(DATA_PATH, "validation_sets", "*", "training.csv"))
-    ids_w_train_and_pairs = []
-    ids = os.listdir(os.path.join(DATA_PATH, "validation_sets"))
+    bench_csvs = glob(os.path.join(DATA_PATH, "validation_sets", "*", "bench.csv"))
 
-    for id_ in ids:
-        if os.path.exists(
-            os.path.join(DATA_PATH, "validation_sets", id_, "training.csv")
-        ) and os.path.exists(
-            os.path.join(DATA_PATH, "validation_sets", id_, "pairs.csv")
-        ):
-            ids_w_train_and_pairs.append(id_)
+    percentages = []
 
-    common_perc = {}
+    for bench_csv in tqdm(bench_csvs):
+        dirname = os.path.dirname(bench_csv)
+        train_csv = os.path.join(dirname, "training.csv")
+        train_wo_csv= os.path.join(dirname, "training_wo_pairs.csv")
 
-    for id_ in tqdm(ids_w_train_and_pairs):
-        training_df = pd.read_csv(
-            os.path.join(DATA_PATH, "validation_sets", id_, "training.csv")
-        )
-        pairs_df = pd.read_csv(
-            os.path.join(DATA_PATH, "validation_sets", id_, "pairs.csv")
-        )
-        pairs = []
-        pairs.extend(pairs_df["smiles_i"])
-        pairs.extend(pairs_df["smiles_j"])
-        pairs = np.unique(pairs)
+        if os.path.exists(train_csv) and os.path.exists(train_wo_csv):
+            bench_df = pd.read_csv(bench_csv)
+            train_df = pd.read_csv(train_csv)
+            train_wo_df = pd.read_csv(train_wo_csv)
 
-        inchis_std_train = set(
-            [
-                MolToInchi(rdMolStandardize.Cleanup(MolFromSmiles(sm)))
-                for sm in training_df["canonical_smiles"]
-            ]
-        )
-        inchis_std_pairs = [
-            MolToInchi(rdMolStandardize.Cleanup(MolFromSmiles(sm))) for sm in pairs
-        ]
+            percentages.append((len(train_df) - len(train_wo_df)) / len(bench_df))
 
-        count_common = 0
-        for inchi_pair in inchis_std_pairs:
-            if inchi_pair in inchis_std_train:
-                count_common += 1
-        common_perc[id_] = count_common / len(inchis_std_pairs)
-
-    with open(os.path.join(RESULTS_PATH, "train_pairs_overlap.pt"), "wb") as handle:
-        dill.dump(common_perc, handle)
+    np.save(os.path.join(RESULTS_PATH, "overlap.npy"), arr=percentages)
--- xaibench/plots/desc.py
+++ xaibench/plots/desc.py
@@ -5,7 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from xaibench.utils import DATA_PATH, FIG_PATH
+from xaibench.utils import DATA_PATH, FIG_PATH, RESULTS_PATH
 
 LIMIT_PSIZE = 5000
 BINSIZE = 50
@@ -20,6 +20,8 @@ plt.rcParams.update(
 if __name__ == "__main__":
     pairs = glob(os.path.join(DATA_PATH, "validation_sets", "*", "pairs.csv"))
     psizes = np.array([len(pd.read_csv(p)) for p in pairs])
+
+    overlaps = np.load(os.path.join(RESULTS_PATH, "overlap.npy"))
 
     tsizes = []
     tsizes_wopairs = []
@@ -43,18 +45,22 @@ if __name__ == "__main__":
     tsizes = np.array(tsizes)
     twosizes = np.array(tsizes_wopairs)
 
-    f, axs = plt.subplots(nrows=2, ncols=1, tight_layout=True, figsize=(4, 7))
+    f, axs = plt.subplots(nrows=3, ncols=1, tight_layout=True, figsize=(4, 10))
     axs[0].hist(psizes[psizes < LIMIT_PSIZE], bins=BINSIZE)
-    xlabels = ['{:d}'.format(int(x)) + 'K' for x in axs[0].get_xticks() / 1000]
-    axs[0].set_xlabel("Benchmark pairs per target")
+    xlabels = [r'{:d}'.format(int(x)) + r'K' for x in axs[0].get_xticks() / 1000]
+    axs[0].set_xlabel(r"Benchmark pairs per target")
     axs[0].set_xticklabels(xlabels)
 
-    axs[1].hist(tsizes, bins=BINSIZE, label="Including pairs")
-    xlabels = ['{:d}'.format(int(x)) + 'K' for x in axs[1].get_xticks() / 1000]
-    axs[1].hist(twosizes, bins=BINSIZE, alpha=0.5, label="Excluding pairs")
-    axs[1].set_xlabel("Training samples per target")
+    axs[1].hist(tsizes, bins=BINSIZE)
+    xlabels = [r'{:d}'.format(int(x)) + r'K' for x in axs[1].get_xticks() / 1000]
+    axs[1].hist(twosizes, bins=BINSIZE, alpha=0.5, label=r"Excl. bench. compounds")
+    axs[1].set_xlabel(r"Training samples per target")
     axs[1].set_xticklabels(xlabels)
     axs[1].legend()
 
+    axs[2].hist(np.array(overlaps) * 100, bins=BINSIZE)
+    axs[2].set_xlabel(r"Benchmark compounds present in the training sets (\%)")
+
+    plt.tight_layout()
     plt.savefig(os.path.join(FIG_PATH, "pairstraining.pdf"))
     plt.close()
