ecef6a1064d5a3763f381a6ac6cc2b8efa96e1a8
Jose Jimenez-Luna <hawk31@gmail.com>
2021-11-09 11:07:38 +0100

    r1 aggregated color direction

--- xaibench/score.py
+++ xaibench/score.py
@@ -39,6 +39,47 @@ def color_agreement(color_true, color_pred, metric_f):
     return metric_f(color_true_noncommon, color_pred_noncommon)
 
 
+def aggregated_color_direction(
+    color_pred_i, color_pred_j, color_true_i, color_true_j, diff
+):
+    """
+    Checks whether the average assigned colors to a substructural
+    change matches the sign of the experimental difference
+    """
+    assert len(color_true_i) == len(color_pred_i)
+    assert len(color_true_j) == len(color_pred_j)
+
+    idx_noncommon_i = [idx for idx, val in color_true_i.items() if val != 0.0]
+    idx_noncommon_j = [idx for idx, val in color_true_j.items() if val != 0.0]
+
+    color_pred_i_noncommon = np.array(
+        [color_pred_i[idx] for idx in idx_noncommon_i]
+    ).flatten()
+    color_pred_j_noncommon = np.array(
+        [color_pred_j[idx] for idx in idx_noncommon_j]
+    ).flatten()
+
+    # If any of these arrays are empty (ie one molecule is a substructure of the other)
+    # assign 0 to the mean
+
+    if len(color_pred_i_noncommon) == 0:
+        color_pred_i_noncommon = np.zeros(1)
+
+    if len(color_pred_j_noncommon) == 0:
+        color_pred_j_noncommon = np.zeros(1)
+
+    i_higher_j = np.mean(color_pred_i_noncommon) > np.mean(color_pred_j_noncommon)
+
+    if diff > 0 and i_higher_j:
+        score = 1.0
+
+    elif diff < 0 and not i_higher_j:
+        score = 1.0
+    else:
+        score = 0.0
+    return score
+
+
 def distribute_bonds(cm, mol, read_f=None):
     """
     Distributes bond importances evenly across their connecting nodes.
@@ -60,6 +101,7 @@ def method_comparison(
 ):
     avg_acc = {}
     avg_f1 = {}
+    avg_direction = {}
     idx_valid = {}
     sizes_valid = {}
 
@@ -72,9 +114,10 @@ def method_comparison(
         with open(color_method_f, "rb") as handle:
             manual_colors = dill.load(handle)
 
+        pair_df = pd.read_csv(os.path.join(dirname, "pairs.csv"))
+
         # If bond importances are used, bond information from the mols is needed
         if assign_bonds:
-            pair_df = pd.read_csv(os.path.join(dirname, "pairs.csv"))
             mols = [
                 (MolFromSmiles(mi), MolFromSmiles(mj))
                 for mi, mj in zip(pair_df["smiles_i"], pair_df["smiles_j"])
@@ -116,9 +159,10 @@ def method_comparison(
                 if sum(1 for _ in filter(None.__ne__, colors_th)) > 0:
                     accs = []
                     f1s = []
+                    directions = []
 
-                    for color_pair_true, color_pair_pred in zip(
-                        colors_th, colors_method
+                    for color_pair_true, color_pair_pred, diff in zip(
+                        colors_th, colors_method, pair_df["diff"].to_list()
                     ):
                         if color_pair_true is not None:
                             acc_i = color_agreement(
@@ -149,11 +193,22 @@ def method_comparison(
                             )
                             f1s.append(f1_j)
 
+                            direction = aggregated_color_direction(
+                                color_pair_pred[0],
+                                color_pair_pred[1],
+                                color_pair_true[0],
+                                color_pair_true[1],
+                                diff,
+                            )
+                            directions.append(direction)
+
                     accs = np.array(accs)
                     accs = accs[accs >= 0.0]  # Filter examples with non-common MCS
 
                     f1s = np.array(f1s)
                     f1s = f1s[f1s >= 0.0]
+
+                    directions = np.array(directions)
 
                     if accs.size > 0:
                         avg_acc.setdefault(
@@ -168,7 +223,11 @@ def method_comparison(
                         sizes_valid.setdefault(
                             method_name, [[] for _ in range(N_THRESHOLDS)]
                         )[idx_th].append(len(accs))
-    return avg_acc, avg_f1, idx_valid, sizes_valid
+
+                    avg_direction.setdefault(
+                        method_name, [[] for _ in range(N_THRESHOLDS)]
+                    )[idx_th].append(directions.mean())
+    return avg_acc, avg_f1, avg_direction, idx_valid, sizes_valid
 
 
 if __name__ == "__main__":
@@ -180,6 +239,12 @@ if __name__ == "__main__":
 
     os.makedirs(RESULTS_PATH, exist_ok=True)
     os.makedirs(FIG_PATH, exist_ok=True)
+
+    accs = {}
+    f1s = {}
+    directions = {}
+    idxs = {}
+    sizes = {}
 
     colors_rf = sorted(
         glob(
@@ -196,17 +261,20 @@ if __name__ == "__main__":
         )
     )
 
-    accs = {}
-    f1s = {}
-    idxs = {}
-    sizes = {}
-
-    accs["rf"], f1s["rf"], idxs["rf"], sizes["rf"] = method_comparison(
-        colors_rf, other_name="rf"
-    )
-    accs["dnn"], f1s["dnn"], idxs["dnn"], sizes["dnn"] = method_comparison(
-        colors_dnn, other_name="dnn"
-    )
+    (
+        accs["rf"],
+        f1s["rf"],
+        directions["rf"],
+        idxs["rf"],
+        sizes["rf"],
+    ) = method_comparison(colors_rf, other_name="rf")
+    (
+        accs["dnn"],
+        f1s["dnn"],
+        directions["dnn"],
+        idxs["dnn"],
+        sizes["dnn"],
+    ) = method_comparison(colors_dnn, other_name="dnn")
 
     for bt in BLOCK_TYPES:
         print(f"Now loading block type {bt}...")
@@ -221,7 +289,7 @@ if __name__ == "__main__":
             )
         )
 
-        accs[bt], f1s[bt], idxs[bt], sizes[bt] = method_comparison(
+        accs[bt], f1s[bt], directions[bt], idxs[bt], sizes[bt] = method_comparison(
             colors_method, avail_methods=avail_methods, assign_bonds=True
         )
 
@@ -230,6 +298,11 @@ if __name__ == "__main__":
 
     with open(os.path.join(RESULTS_PATH, f"f1s{args.savename}.pt"), "wb") as handle:
         dill.dump(f1s, handle)
+
+    with open(
+        os.path.join(RESULTS_PATH, f"directions{args.savename}.pt"), "wb"
+    ) as handle:
+        dill.dump(directions, handle)
 
     with open(os.path.join(RESULTS_PATH, f"idxs{args.savename}.pt"), "wb") as handle:
         dill.dump(idxs, handle)
